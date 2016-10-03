ec1a84072ace70696c99805ee4219e8467c090d8
iykef <iykef@LAPTOP-BIFVKPS5>
2016-10-02 22:40:47 -0700

    FizzBuzz Project

---
+++ Homework 09_27/Homework 09_27/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_09_27
+{
+    class Program
+    {
+        static int uservalue;
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                bool fizz = i % 3 == 0;
+                bool buzz = i % 5 == 0;
+                if (fizz && buzz)
+                    Console.WriteLine("FizzBuzz");
+                else if (fizz)
+                    Console.WriteLine("Fizz");
+                else if (buzz)
+                    Console.WriteLine("Buzz");
+                else
+                    Console.WriteLine(i);
+
+            }
+                    Console.ReadLine();
+        }
+    }
+}
---
+++ Interactive Fizz Buzz Project/Interactive Fizz Buzz Project/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interactive_Fizz_Buzz_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = ("Brainiac Fizz Buzz v1");
+            Console.WriteLine("Hello I am Brainiac.\n");
+            Console.WriteLine("Pick a number between 1 and 100\n");
+            Console.WriteLine("I will yell fizz for a number divisible by 3 and buzz for a number divisible by 5\n");
+            Console.WriteLine(" and fizz buzz for a number divisible by both\n");
+            Console.Write("Try me.\n");
+            string UserValue = Console.ReadLine();
+            int newNum = Convert.ToInt32(UserValue);
+            if (newNum % 3 == 0 && newNum % 5 == 0)
+            {
+                Console.WriteLine("Fizz Buzz");
+            }
+            else if (newNum % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (newNum % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine("oops you are out of bounds!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+        
+    
+
